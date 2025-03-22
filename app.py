c7397cea6bbf8c9cfe5634c6dd671220d89fc74d
Tamnemng <nguyndantdm6@gmail.com>
2025-03-23 01:55:14 +0700

    change port

--- app.py
+++ app.py
@@ -191,5 +191,4 @@ def delete_item(key):
     return redirect(url_for('view_key', key=key))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5000)
-    
+    app.run(host="0.0.0.0", debug=True, port=5678)
