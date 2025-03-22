21ca56540d080ba146312154a1b583f7394b2dd6
Tamnemng <nguyndantdm6@gmail.com>
2025-03-23 01:08:30 +0700

    add host

--- app.py
+++ app.py
@@ -191,4 +191,4 @@ def delete_item(key):
     return redirect(url_for('view_key', key=key))
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", debug=True, port=5000)
